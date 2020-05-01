81bc2b1108b200f2d94de7469d305420854a25a4
Abdullah Hadi <sir-hadi@users.noreply.github.com>
2020-05-01 17:53:26 +0700

    Somehow Bisa

--- src/main/java/Congklak.java
+++ src/main/java/Congklak.java
@@ -1,4 +1,5 @@
-import java.util.Arrays; 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -25,24 +26,35 @@ public class Congklak {
         n = scan.nextInt();
         System.out.print("Input number of seed in holes : ");
         s = scan.nextInt();
-        
+
         board = new int[(n * 2)];
         emptyBoard = new int[(n * 2)];
-        
+
         //fills board with s number on every element/index array
         fillBoard(board, s);
         fillBoard(emptyBoard, 0);
-        
+
         //Loop while arrays value is not 0 on every element/index array
         while (!Arrays.equals(board, emptyBoard)) {
+//           if (!Arrays.equals( Arrays.copyOfRange(board, 0, (board.length/2)-1) , Arrays.copyOfRange(emptyBoard, 0, (emptyBoard.length/2)-1))) {
             play(1);
+//            }
             System.out.println("====================");
             viewBoard();
             System.out.println("====================");
+//            if (!Arrays.equals( Arrays.copyOfRange(board,(board.length/2) , board.length-1) , Arrays.copyOfRange(emptyBoard,(emptyBoard.length/2) , emptyBoard.length-1)) ) {
             play(2);
+//           }
             System.out.println("====================");
             viewBoard();
             System.out.println("====================");
+        }
+        if (score1>score2){
+            System.out.println(score1 +":"+score2);
+            System.out.println("Player 1 Won");
+        }else{
+            System.out.println(score1 +":"+score2);
+            System.out.println("Player 2 Won");
         }
 
     }
@@ -62,43 +74,48 @@ public class Congklak {
     private static void play(int player) {
         boolean playing = true;
         int pos = choosePos(player); // choose first pos
+
         int hand = board[pos]; // fill hand from pos
-        board[pos] = 0; // empty the board pos
-        while (playing) { // will keep going until playing 0
-            if (pos < board.length - 1) {
-                pos++; // pos next            
-            } else {
-                pos = 0;
-            }
-            hand--; // hand -1
-            board[pos]++; // board +1
-            if (player == 1 && pos == (n - 1) && hand > 0) { // check when a. player 1, b. exactly on n-1, c. hand not
-                // empty
-                score1++; // add score
-                hand--; // hand -1
-                if (hand <= 0) { // if hand became empty, take another position
-                    pos = choosePos(player);
-                    hand = board[pos];
-                    board[pos] = 0;
+        if (!(hand <= 0)) { //the hand was -671689 somehow
+            board[pos] = 0; // empty the board pos
+            while (playing) { // will keep going until playing 0
+                if (player == 1 && pos == (n - 1) && hand > 0) { // check when a. player 1, b. exactly on n-1, c. hand not
+                    // empty
+                    score1++; // add score
+                    hand--; // hand -1
+                    if (hand <= 0 ) { // if hand became empty, take another position
+                        pos = choosePos(player);
+                        hand = board[pos];
+                        board[pos] = 0;
+                    }
+                } else if (player == 2 && pos == (2 * n - 1) && hand > 0) { // check when a. player 2, b. exactly on 2n-1,
+                    // c. hand not empty
+                    score2++; // add score
+                    hand--; // hand -1
+                    if (hand <= 0 ) { // if hand became empty, take another position
+                        pos = choosePos(player);
+                        hand = board[pos];
+                        board[pos] = 0;
+                    } else {
+                        pos = 0; // edge of board (making sure no overload) // change it to 0 biar gak out of bound kalo di cek di if stament yang bawah
+                    }
                 }
-            } else if (player == 2 && pos == (2 * n - 1) && hand > 0) { // check when a. player 2, b. exactly on 2n-1,
-                // c. hand not empty
-                score2++; // add score
-                hand--; // hand -1
-                if (hand <= 0) { // if hand became empty, take another position
-                    pos = choosePos(player);
-                    hand = board[pos];
-                    board[pos] = 0;
-                } else {
-                    pos = -1; // edge of board (making sure no overload)
-                }
-            }
-            if (hand == 0) { // when hand became empty
-                if (board[pos] > 1) { // if the current board position bigger than 1 (previously not 0) take them all
-                    hand = board[pos];
-                    board[pos] = 0;
-                } else { // else its over
-                    playing = false;
+
+                if (hand <= 0) { // when hand became empty
+                    if (board[pos] > 1) { // if the current board position bigger than 1 (previously not 0) take them all
+                        hand = board[pos];
+                        board[pos] = 0;
+                    } else { // else its over
+                        playing = false;
+                    }
+                }else{
+                    if (pos < board.length - 1) {
+                        pos++; // pos next
+                    } else {
+                        pos = 0;
+                    }
+                    hand--; // hand -1
+                    board[pos]++; // board +1
                 }
             }
         }
@@ -108,12 +125,12 @@ public class Congklak {
         int start, end;
         if (player == 1) {
             start = 0;
-            end = n; // biar gak out of bound arraynya
+            end = n;
         } else {
             start = n;
-            end = (2 * n); // biar gak out of bound arraynya
+            end = (2 * n);
         }
-        int max = 0;
+        int max = start; // change it to start, biar dibandinginnya sama elemen punya player
         for (int i = start; i < end; i++) {
             if (board[i] > board[max]) {
                 max = i;
